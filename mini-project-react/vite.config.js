e8428febedc791fac2004a5e994b9c5eddd1f145
Nitesh9162 <soninitesh31049@gmail.com>
2026-09-11 10:55:26 +0530

    Configure base path for deployment

--- mini-project-react/vite.config.js
+++ mini-project-react/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     open: false, // Prevents Vite from automatically opening a browser window on npm run dev
   }
